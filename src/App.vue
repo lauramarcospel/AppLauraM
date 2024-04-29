4e775d870ad34b47df1163a99e8a709260f42976
lauramarcospel <lauramarcosces@gmail.com>
2024-04-29 11:47:51 +0200

    HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-
+import HelloWorld from "./components/HelloWorld.vue"
 </script>
 
 <template>
  <h1 class="font-bold text-xl text-red-600 underline">
     Hello world!
   </h1> 
+  <HelloWorld />
 </template>
 
